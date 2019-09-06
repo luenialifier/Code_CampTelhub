fe4700a8b12e84ef22645cd4000deaa283c996f2
luenial <luenial380@localhost.localdomain>
2019-09-06 12:20:01 -0500

    agregando build y dependencias

---
+++ build/precache-manifest.ce7348f968137e25670a995f198fc25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c39218cdf5e409897b47ebcc5e5f8d9",
+    "url": "/len/index.html"
+  },
+  {
+    "revision": "0674ca5e6ec6ffc5a85b",
+    "url": "/len/static/css/2.5d45f07d.chunk.css"
+  },
+  {
+    "revision": "5f28f7b188aaa1bd0bd5",
+    "url": "/len/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0674ca5e6ec6ffc5a85b",
+    "url": "/len/static/js/2.07489c9d.chunk.js"
+  },
+  {
+    "revision": "5f28f7b188aaa1bd0bd5",
+    "url": "/len/static/js/main.304e6505.chunk.js"
+  },
+  {
+    "revision": "10936e944ad042de778a",
+    "url": "/len/static/js/runtime~main.c5d1dfe3.js"
+  }
+]);
